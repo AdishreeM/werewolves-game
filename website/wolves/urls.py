388ad232937b9dd53f1ab6550b2cf151ebc0f3ef
Adishree Mazumder <adishreemaz09@gmail.com>
2019-05-25 22:42:06 +0530

    Fixed errors in room templates

--- website/wolves/urls.py
+++ website/wolves/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('room', views.room, name='room')
+    path('room', views.room, name='room'),
+    path('table', views.table, name='table')
 ]
